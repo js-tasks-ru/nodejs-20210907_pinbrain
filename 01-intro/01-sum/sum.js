440db0b651174cbd8281c2facc9b92cdf3d806f9
Pavel Kuzikov <pinbrain@yandex.ru>
2021-09-04 22:35:10 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  let sum = 0;
+  for (let arg of arguments){
+    if(typeof arg !== 'number'){
+      throw new TypeError ('Аргументы не числа!');
+    }else{
+      sum += arg;
+    }
+  }
+  return sum;
 }
 
 module.exports = sum;
