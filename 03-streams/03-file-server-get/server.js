8ce6c138c2fb0de6b0c390d1b939b4b94f703292
Pavel Kuzikov <pinbrain@yandex.ru>
2021-09-16 11:46:49 +0300

    add solution for module 3 task 2-5

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,39 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  function getFile(){
+    if(pathname.length > 0 && pathname.indexOf('/') === -1){
+      const filepath = path.join(__dirname, 'files', pathname);
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
+      stream.on('error', (error) => {
+        if(error.code = 404){
+          res.statusCode = 404;
+          res.end('File not found');
+        }else{
+          res.statusCode = 500;
+          res.end('Something go wrong...');
+        }
+      });
+      stream.on('aborted', () => {
+        stream.destroy();
+      });
+    }else{
+      res.statusCode = 400;
+      res.end('Bad request url');
+    }
+  }
 
   switch (req.method) {
     case 'GET':
-
+      getFile();
       break;
 
     default:
--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,11 +9,34 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  function deleteFile() {
+    if(pathname.length > 0 && pathname.indexOf('/') === -1){
+      const filepath = path.join(__dirname, 'files', pathname);
+      fs.stat(filepath, (err, stats) => {
+        if(err || !stats.isFile()) {
+          res.statusCode = 404;
+          res.end('file not found');
+        }else{
+          fs.unlink(filepath, (err) => {
+              if(err){
+                  res.statusCode = 500;
+                  res.end('Server error')                        
+              }else{
+                  res.statusCode = 200;
+                  res.end('File was deleted')
+              }
+          });
+        }
+      });
+    }else{
+      res.statusCode = 400;
+      res.end('Bad request url');
+    }
+  }
 
   switch (req.method) {
     case 'DELETE':
-
+      deleteFile();
       break;
 
     default:
