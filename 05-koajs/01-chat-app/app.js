5a1631b70f4503d5a36c3a27e9c0f3f62cf5abdf
Pavel Kuzikov <pinbrain@yandex.ru>
2021-09-28 23:26:18 +0300

    add solution for module 5 task 1

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -7,11 +7,22 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const subscribers = new Map();
 
 router.get('/subscribe', async (ctx, next) => {
+    const subId = Math.random();
+    ctx.res.on('close', () => {
+        subscribers.delete(subId);
+    });
+    const msg = await new Promise(resolve => subscribers.set(subId, resolve));
+    ctx.body = msg;
 });
 
 router.post('/publish', async (ctx, next) => {
+    const msg = ctx.request.body.message;
+    if(!msg) ctx.throw(400, 'no message found')
+    subscribers.forEach(resolve => resolve(msg));
+    ctx.body = msg;
 });
 
 app.use(router.routes());
