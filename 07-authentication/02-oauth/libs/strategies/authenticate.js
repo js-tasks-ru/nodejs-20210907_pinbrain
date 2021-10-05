9ab36c10d7c47711fec9148a8ab952fcc8176bff
Pavel Kuzikov <pinbrain@yandex.ru>
2021-10-06 00:20:52 +0300

    add solution for module 7 task 2 - corrected

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -3,14 +3,18 @@ const Users = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
   console.log(email, displayName);
-  if(!email) return done(null, false, 'Отсутствует email');
+  if(!email) return done(null, false, 'Не указан email');
   let user = await Users.findOne({email: email});
   if(!user) {
-    user = new User({
-      email: email,
-      displayName: (displayName) ? displayName : email.substr(0, email.indexOf('@'))
-    });
-    await user.save();
+    try{
+      user = new User({
+        email: email,
+        displayName: (displayName) ? displayName : email.substr(0, email.indexOf('@'))
+      });
+      await user.save();
+    } catch(err) {
+      return done(err);
+    }
   }
   done(null, user);
 };
