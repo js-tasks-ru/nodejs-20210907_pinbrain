f2d506f525dfdc9c8e84fc15714cd34633202ca3
Pavel Kuzikov <pinbrain@yandex.ru>
2021-10-17 12:57:08 +0300

    add solution for module 9 task 1

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,13 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  const messages = await Message.find({chat: ctx.user.id}).sort({date: 'desc'}).limit(20);
+  ctx.body = {messages: messages.map(msg => {
+    return {
+      date: msg.date,
+      text: msg.text,
+      id: msg._id,
+      user: msg.user
+    }
+  })};
 };
