9457a437d329a6e5bfd7c9e249f7137d1644e52b
Sergey Zelenov <zelenov@so1.ai>
2021-11-03 12:26:21 +0100

    update mongoose & validation plugin

--- 09-websockets/01-chat/libs/connection.js
+++ 09-websockets/01-chat/libs/connection.js
@@ -4,9 +4,4 @@ const config = require('../config');
 
 mongoose.plugin(beautifyUnique);
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
 module.exports = mongoose.createConnection(config.mongodb.uri);
